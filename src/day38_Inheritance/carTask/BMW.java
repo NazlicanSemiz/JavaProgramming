96c102b0ba0fcdfcd9d7a732d2ccffa9b81259f8
Nazlican Semiz <nazlicankucuksabanoglu@gmail.com>
2022-12-16 00:33:30 +0000

    day38: Inheritance continue

--- src/day38_Inheritance/carTask/BMW.java
+++ src/day38_Inheritance/carTask/BMW.java
@@ -1,6 +1,18 @@
 package day38_Inheritance.carTask;
 
-public class BMW {
+public class BMW extends Car {
+
+    public BMW(String brand, String model, int year, double price, String color, int miles) {
+        super(brand, model, year, price, color, miles);
+    }
+
+    public void breaksDown(){
+        System.out.println(brand+ " "+model+ " breaks down every 100 miles");
+    }
+
+    public void racing(){
+        System.out.println(brand+ " "+ model+ " racing");
+    }
 
 }
 
