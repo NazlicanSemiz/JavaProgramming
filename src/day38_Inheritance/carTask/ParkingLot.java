aea2803830ea8994cac73f1c732674b8d9dbd327
Nazlican Semiz <nazlicankucuksabanoglu@gmail.com>
2022-12-16 01:49:13 +0000

    day38: Inheritance continue

---
+++ src/day38_Inheritance/carTask/ParkingLot.java
@@ -0,0 +1,12 @@
+package day38_Inheritance.carTask;
+
+public class ParkingLot {
+    public static void main(String[] args) {
+
+        Toyota toyota=new Toyota("Toyota", "Yaris",2020, 20000,"Red",123456);
+
+        Tesla tesla= new Tesla("Tesla", "X model",2022,100000,"Red",10000);
+
+        BMW bmw = new BMW("BMW","x5",2022,5000,"White",54665435);
+    }
+}
